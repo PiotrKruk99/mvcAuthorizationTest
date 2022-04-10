e1d74564ecd5577742979a8d528bb23e13aa2531
PiotrKruk99-d <biuro@liberezo.pl>
2022-04-10 23:28:14 +0200

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using mvcTest.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace mvcTest.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [Route("/secrets")]
+    [Authorize]
+    public IActionResult Secrets()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace mvcTest.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
